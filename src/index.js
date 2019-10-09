747254af565c750a12b2d238e390015cba3cb9d5
digitalapplebee <digitalapplebee@gmail.com>
2019-10-09 22:13:04 +0300

    Add PropTypes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import PropTypes from 'prop-types';
 import dyslexiaFunc from './dyslexia';
 
 function Dyslexia({ 
@@ -18,16 +19,30 @@ function Dyslexia({
     setTimeout(
       () =>
         setText(
-          dyslexiaFunc(text || 'Dyslexia', {
-            minWordLength: minWordLength || 2,
-            scrambleChance: scrambleChance || 80
+          dyslexiaFunc(text, {
+            minWordLength: minWordLength,
+            scrambleChance: scrambleChance
           })
         ),
-      delay || 2000
+      delay
     );
   };
 
   return <p style={{ margin: 0, padding: 0 }}>{dyslexiaText}</p>;
+}
+
+Dyslexia.defaultProps = {
+  text: "Dyslexia",
+  delay: 2000,
+  minWordLength: 2,
+  scrambleChance: 80,
+}
+
+Dyslexia.propTypes = {
+  text: PropTypes.string,
+  delay: PropTypes.number,
+  minWordLength: PropTypes.number,
+  scrambleChance: PropTypes.number,
 }
 
 export default Dyslexia;
